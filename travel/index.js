da463368b78b2a69fa4eea6d13eda04207b184c5
zEtYxA <zmaxet@mail.ru>
2022-07-11 00:43:48 +0300

    change with figma

--- travel/index.js
+++ travel/index.js
@@ -1,1 +1,14 @@
 console.log('hello');
+
+//burger handler
+(function () {
+   const burgeritem = document.querySelector('.burger');
+   const menu = document.querySelector('.header__nav');
+   const burgeritemclose = document.querySelector('.header__nav-close');
+   burgeritem.addEventListener('click', () => {
+      menu.classList.add('header__nav_active');
+   })
+   burgeritemclose.addEventListener('click', () => {
+      menu.classList.remove('header__nav_active');
+   })
+}());
