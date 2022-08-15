f06a9d15e2eb2b1662f4c12b7c52054b93c76c11
zEtYxA <zmaxet@mail.ru>
2022-08-16 00:55:39 +0300

    change 1608

--- js/script.js
+++ js/script.js
@@ -6,27 +6,63 @@ const city = document.querySelector('.city');
 const body = document.body;
 const slideNext = document.querySelector('.slide-next');
 const slidePrev = document.querySelector('.slide-prev');
+const weather = document.querySelector('.weather');
 const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
 const weatherDescription = document.querySelector('.weather-description');
 const quote = document.querySelector('.quote');
 const author = document.querySelector('.author');
 const changeQuote = document.querySelector('.change-quote');
+const player = document.querySelector('.player');
 const play = document.querySelector('.play');
 const playNext = document.querySelector('.play-next');
 const playPrev = document.querySelector('.play-prev');
+const timeBalance = document.getElementById("time");
+const dateBalance = document.getElementById("date");
+const audioeBalance = document.getElementById("audio");
+const weatherBalance = document.getElementById("weather");
+const quotesBalance = document.getElementById("quotes");
+const greetingBalance = document.getElementById("greeting");
+const GitHubBalance = document.getElementById("GitHub");
+const UnsplashBalance = document.getElementById("Unsplash");
+const FlickrBalance = document.getElementById("Flickr");
+const en = document.getElementById("en");
+const rus = document.getElementById("rus");
+const lang = document.querySelectorAll('.lang');
+
+
+
+
+const rechange = document.querySelectorAll('.rechange');
+let randomNum = '01';
+let n = 18;
+let language = 0;
+let ap = 0;
+
+
+
 let playNum = 0;
 
 let isPlay = false;
 const data = new Date();
 const currentTime = data.toLocaleTimeString();
 const options = { weekday: 'long', month: 'long', day: 'numeric', timeZone: 'UTC' };
-const currentDate = data.toLocaleDateString('en-EN', options);
-let randomNum = '01';
+let currentDate = data.toLocaleDateString('en-EN', options);
+function datelang() {
+   console.log('asda');
+   if (language == 0) {
+      currentDate = data.toLocaleDateString('en-EN', options);
+   }
+   if (language == 1) {
+      currentDate = data.toLocaleDateString('ru-RU', options);
+   }
+
+}
+//datelang()
+
 const timeOfDay = getTimeOfDay();
 const greetingText = `Good ${timeOfDay}`;
 greeting.textContent = greetingText;
-let n = 18;
 
 
 
@@ -34,13 +70,153 @@ let n = 18;
 //body.style.background = `url('https://github.com/zEtYxA/stage1-tasks/tree/assets/images/${timeOfDay}/${randomNum}.jpg') center/cover, rgba(0, 0, 0, 0.5)`
 
 city.value = 'Minsk';
+function balancetime() {
+
+   if (!timeBalance.checked) {
+      time.style.display = 'none';
+   } else {
+      time.style.display = 'block';
+
+   }
+}
+
+timeBalance.addEventListener('change', balancetime)
+
+function balancedate() {
+
+   if (!dateBalance.checked) {
+      date.style.display = 'none';
+   } else {
+      date.style.display = 'block';
+
+   }
+}
+
+dateBalance.addEventListener('change', balancedate)
+
+function balanceplay() {
+
+   if (!audioeBalance.checked) {
+      player.style.display = 'none';
+   } else {
+      player.style.display = 'block';
+
+   }
+}
+
+audioeBalance.addEventListener('change', balanceplay)
+
+function balanceweather() {
+
+   if (!weatherBalance.checked) {
+      weather.style.display = 'none';
+   } else {
+      weather.style.display = 'block';
+
+   }
+}
+
+weatherBalance.addEventListener('change', balanceweather)
+
+function balancquotes() {
+
+   if (!quotesBalance.checked) {
+      quote.style.display = 'none';
+      author.style.display = 'none';
+   } else {
+      quote.style.display = 'block';
+      author.style.display = 'block';
+
+   }
+}
+
+quotesBalance.addEventListener('change', balancquotes)
+function balancegreeting() {
+
+   if (!greetingBalance.checked) {
+      greeting.style.display = 'none';
+
+   } else {
+      greeting.style.display = 'block';
 
 
+   }
+}
 
-function greetingTranslation(lang) {
+greetingBalance.addEventListener('change', balancegreeting)
 
+function api() {
+
+   if (GitHubBalance.checked) {
+      ap = 0;
+      UnsplashBalance.checked = false;
+      FlickrBalance.checked = false;
+      setBg()
+   }
+}
+function api1() {
+
+   if (UnsplashBalance.checked) {
+      ap = 1;
+      GitHubBalance.checked = false;
+      FlickrBalance.checked = false;
+      setBg()
+   }
 
 }
+function api2() {
+
+
+   if (FlickrBalance.checked) {
+      ap = 2;
+      GitHubBalance.checked = false;
+      UnsplashBalance.checked = false;
+      setBg()
+   }
+}
+
+
+
+GitHubBalance.addEventListener('change', api)
+UnsplashBalance.addEventListener('change', api1)
+FlickrBalance.addEventListener('change', api2)
+
+
+
+function langechangeen() {
+
+
+   if (en.checked) {
+
+      language = 0;
+      rus.checked = false;
+      getWeather()
+      getQuotes()
+      datelang()
+   }
+
+}
+en.addEventListener('change', langechangeen)
+function langechangerus() {
+
+
+   if (rus.checked) {
+      language = 1
+      en.checked = false
+      getWeather()
+      getQuotes()
+      datelang()
+   }
+
+}
+rus.addEventListener('change', langechangerus)
+
+
+//const GitHubBalance = document.getElementById("GitHub");
+//const UnsplashBalance = document.getElementById("Unsplash");
+//const FlickrBalance = document.getElementById("Flickr");
+
+
 
 //greetingTranslation()
 
@@ -54,8 +230,9 @@ function showTime() {
 showTime();
 function showDate() { date.textContent = currentDate; }
 function getTimeOfDay() {
-   const hours = data.getHours()
-   const timeDay = ['night', 'morning', 'afternoon', 'evening'];
+   const hours = data.getHours();
+   const timeDay = ['night', 'morning', 'afternoon', 'evening']
+
    //const timeDay = ['ночи', 'утра', 'дня', 'вечера'];
    if (Math.trunc(hours / 6) < 1) { return timeDay[0] }
    if (Math.trunc(hours / 6) < 2) { return timeDay[1] }
@@ -69,7 +246,7 @@ function getRandomNum() {
 }
 getRandomNum()
 
-let ap = 2;
+
 function setBg() {
    let timeOfDay = getTimeOfDay()
    const img = new Image();
@@ -134,29 +311,55 @@ function getLocalStorage() {
 }
 
 async function getWeather() {
-
-   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=80c9e01c478e778c841796a3b53ed1d9&units=metric`;
-   const res = await fetch(url);
-   const data = await res.json();
-   weatherIcon.className = 'weather-icon owf';
-   weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-   temperature.textContent = `${data.main.temp}°C`;
-   weatherDescription.textContent = data.weather[0].description;
+   if (language == 0) {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=80c9e01c478e778c841796a3b53ed1d9&units=metric`;
+      const res = await fetch(url);
+      const data = await res.json();
+      weatherIcon.className = 'weather-icon owf';
+      weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+      temperature.textContent = `${data.main.temp}°C`;
+      weatherDescription.textContent = data.weather[0].description;
+   }
+   if (language == 1) {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=80c9e01c478e778c841796a3b53ed1d9&units=metric`;
+      const res = await fetch(url);
+      const data = await res.json();
+      weatherIcon.className = 'weather-icon owf';
+      weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+      temperature.textContent = `${data.main.temp}°C`;
+      weatherDescription.textContent = data.weather[0].description;
+   }
 
 }
+getWeather()
+
 
 
 async function getQuotes() {
-   const quotes = 'data.json';
-   const res = await fetch(quotes);
-   const data = await res.json();
-   let length = data.length;
-   let num = Math.floor(Math.random() * length)
-   //if (num == 0) { num = length }
+   if (language == 0) {
+      const quotes = 'data.json';
+      const res = await fetch(quotes);
+      const data = await res.json();
+      let length = data.length;
+      let num = Math.floor(Math.random() * length);
+      //if (num == 0) { num = length }
 
-   quote.textContent = data[num].text;
-   author.textContent = data[num].author;
-   console.log(num);
+      quote.textContent = data[num].text;
+      author.textContent = data[num].author;
+   }
+   if (language == 1) {
+      // console.log('fyfel');
+      const quotes = 'data1.json';
+      const res = await fetch(quotes);
+      const data = await res.json();
+      let length = data.length;
+      let num = Math.floor(Math.random() * length);
+      //if (num == 0) { num = length }
+
+      quote.textContent = data[num].text;
+      author.textContent = data[num].author;
+   }
+
 }
 getQuotes();
 
