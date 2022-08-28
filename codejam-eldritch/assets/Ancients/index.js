7c224a3cd4cdf13c9a59d0f20cc2fdd23ce31245
zEtYxA <zmaxet@mail.ru>
2022-08-28 20:47:52 +0300

    dasd

---
+++ codejam-eldritch/assets/Ancients/index.js
@@ -0,0 +1,13 @@
+let azathoth = '../Ancients/Azathoth.png';
+let cthulhu = '../Ancients/Cthulthu.png';
+let iogSothoth = '../Ancients/IogSothoth.png';
+let shubNiggurath = '../Ancients/ShubNiggurath.png';
+
+const ancients = {
+  azathoth,
+  cthulhu,
+  iogSothoth,
+  shubNiggurath
+}
+
+export default ancients
---
+++ codejam-eldritch/data/mythicCards/index.js
@@ -0,0 +1,10 @@
+import brownCards from './brown/index.js';
+import blueCards from './blue/index.js';
+import greenCards from './green/index.js';
+const allcards = {
+  brownCards,
+  blueCards,
+  greenCards
+}
+
+export default allcards
